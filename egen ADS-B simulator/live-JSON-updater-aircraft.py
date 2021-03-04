2c279da85456d83e51f2439ed7339b457c7e691d
lateSWE <35782260+lateSWE@users.noreply.github.com>
2021-03-04 22:09:00 +0100

    Added some choices for the user to make

--- egen ADS-B simulator/live-JSON-updater-aircraft.py
+++ egen ADS-B simulator/live-JSON-updater-aircraft.py
@@ -9,19 +9,54 @@ def writeToJson(out):
     json.dump(out, write_file)
     write_file.close()
 
+choise = input("Do you want to enter the values manualy? It will fly in a circle around the given coordenates\n(Not recomended)[Y] or [N]: ")
+if  choise == "Y" or choise == "y":
+    Hex         = int(input("Hex: "), 16)
+    Squawk      = float(input("Squawk: "))
+    Flight      = float(input("Flight: "))
+    ELat        = float(input("Lat: "))
+    ELon        = float(input("Lon: "))
+    #SeenPos     = float(input("SeenPos: "))
+    Altitude    = float(input("Altitude: "))
+    VertRate    = float(input("VertRate: "))
+    Track       = float(input("Track: "))
+    Speed       = float(input("Speed: "))
+    Category    = float(input("Category: "))
+    #Mlat        = float(input("Mlat: "))
+    #Messages2   = float(input("Messages: "))
+    Seen        = float(input("Seen: "))
+    Rssi        = float(input("Rssi: "))
+else:
+    print("What premade flight dou you want to use?")
+    print("1: circle around you")
+    print("2: circle around given coordenates")
+    choise = input("Starting number: ")
+if(choise == 1):
+    print("1")
+elif(choise == 2):
+    print("2")
+else:
+    print("ERROR, the number you enterd does not match up to any premade flight!")
+    quit()
+refreshRate = int(input("What refresh rate? "))
 
 while True:
-    Time = round(time.time(), 1)
-    Messages = 200
-    Hex = 500
 
+    #Lat = ELat+radius*cos(angle)
+    #Lon = ELon+radius*sin(angle)
+
+
+
+
+    Time = round(time.time(), 1)
+    Messages1 = Messages2
     String = {
         'now':Time,
-        'messages': Messages,
+        'messages': Messages1,
         'aircraft': [
             {
                 'hex': str(Hex),
-                'squawk': str(Wquawk),
+                'squawk': str(Squawk),
                 'flight': str(Flight),
                 'lat': Lat,
                 'lon': Lon,
@@ -32,12 +67,12 @@ while True:
                 'speed': Speed,
                 'category': str(Category),
                 #'mlat': Mlat,
-                #'messages': Messages,
+                #'messages2': Messages,
                 'seen': Seen,
                 'rssi': Rssi
             }
         ]
     }
-
-print(String)
-writeToJson(String)
+    writeToJson(String)
+    time.sleep(refreshRate)
+    print(String)
